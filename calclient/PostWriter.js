c4d1f7b771ae36f81f53e80492676fc9bea1ea24
Ben Mordue <benmordue@gmail.com>
2019-05-07 14:45:46 +0200

    PostWriter.writePost

--- calclient/PostWriter.js
+++ calclient/PostWriter.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
+const util = require('util');
 
 module.exports = {
-  writePost = function() {}
+	writePost: function(calendarObject) {
+		return new Promise((resolve, reject) => {
+//			console.log('Logging to file: ' + file);
+			var etag = calendarObject.data.props.getetag;
+			var file = 'dump_' + etag.slice(1,-1) + '.json';
+			console.log('Logging to file: ' + file);
 
-
+			var data = util.inspect(calendarObject, { compact: false, depth: 6} );
+			fs.writeFile(file, data, function(err) {
+				err ? reject() : resolve(0);
+//				if (err) { reject() } else { resolve(0); }
+			});
+		});
+	}
 };
